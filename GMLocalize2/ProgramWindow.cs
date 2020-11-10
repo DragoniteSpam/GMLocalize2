f86c225854351a5476bcd4a18d49c84b94b467fa
Dragonite <mpeng@optimum.net>
2020-11-10 17:52:52 -0500

    hello world

---
+++ GMLocalize2/ProgramWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GMLocalize2 {
+    public partial class ProgramWindow : Form {
+        public ProgramWindow() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
