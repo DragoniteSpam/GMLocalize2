e5aaf3da72a69d417113a6f3643661bf6a0e6065
Dragonite <mpeng@optimum.net>
2020-11-10 18:04:11 -0500

    format UI

--- GMLocalize2/ProgramWindow.Designer.cs
+++ GMLocalize2/ProgramWindow.Designer.cs
@@ -23,21 +23,78 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listText = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // buttonOpen
+            // 
+            this.buttonOpen.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOpen.Location = new System.Drawing.Point(12, 66);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(128, 32);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Open Project";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExport.Location = new System.Drawing.Point(146, 66);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(128, 32);
+            this.buttonExport.TabIndex = 1;
+            this.buttonExport.Text = "Export...";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 39);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Welcome to GMLocalize!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listText
+            // 
+            this.listText.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listText.FormattingEnabled = true;
+            this.listText.ItemHeight = 19;
+            this.listText.Location = new System.Drawing.Point(12, 104);
+            this.listText.Name = "listText";
+            this.listText.Size = new System.Drawing.Size(460, 327);
+            this.listText.TabIndex = 4;
+            this.listText.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // ProgramWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 461);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.listText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonExport);
+            this.Controls.Add(this.buttonOpen);
+            this.Name = "ProgramWindow";
+            this.Text = "GMLocalize";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listText;
     }
 }
 
--- GMLocalize2/ProgramWindow.cs
+++ GMLocalize2/ProgramWindow.cs
@@ -17,5 +17,21 @@ namespace GMLocalize2 {
         private void Form1_Load(object sender, EventArgs e) {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
     }
 }
